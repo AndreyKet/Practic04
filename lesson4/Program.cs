896318b85a75285724ef947cbcfad15b2b0e8ca3
Andrey <man.liher66@gmail.com>
2022-11-23 22:57:46 +0300

    Лекция

--- lesson4/Program.cs
+++ lesson4/Program.cs
@@ -1,4 +1,170 @@
-﻿//Шпаргалка:
+﻿// //Вид 1
+
+// void Method1()
+// {
+//     Console.WriteLine("Автор...");
+// }
+// Method1();
+
+// //Вид 2
+
+// void Method2(string msg);
+// {
+//     Console.WriteLine(msg);
+// }
+// Method2("Текст сообщения");
+
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine(msg);
+//         i++;
+//     }
+// }
+// Method21(msg: "Текст", count: 4);
+
+// //Вид 3
+
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+
+// int year = Method3();
+// Console.WriteLine(year);
+
+// //Вид 4
+// string Method4(int count, string c)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+//     while (i < count)
+//     {
+//         result = c + result;
+//         i++;
+//     }
+//     return result;
+// }
+
+// string res = Method4(10, "zz");
+// Console.WriteLine(res);
+
+
+// // Цикл FOR
+
+// for (int i = 0; i < count; i++)
+// {
+//     Console.WriteLine(i);
+// }
+// string Method4(int count, string c)
+// {     
+//     string result = String.Empty;
+//     for (int i = 0; i < count; i++)
+//     {
+//         result = c + result;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "zz");
+// Console.WriteLine(res);
+
+// //Цикл в цикле
+
+// for (int i=2; i<=10; i++)
+// {
+//     for (int j=2; j<=10; i++)
+//     {
+//         Console.WriteLine($"{i} x {j} = {i * j}");
+//     }
+//     Console.WriteLine();
+// }
+
+
+// string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+//             + "ежели вас послали вместо нашего милого Винценгероде,"
+//             + "вы бы взяли приступом согласие прусского короля. "
+//             + "Вы так красноречивы. Вы дадите мне чаю?";
+// // string s = "qwerty"
+// //            [0123..]
+// // s[3] // r
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = String.Empty;
+//     int length = text.Length;
+//     for (int i = 0; i < length; i++)
+//     {
+//         if(text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }    
+//     return result;
+// }
+// string newText = Replace(text, ' ', '|');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'к', 'К');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'с', 'С');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+
+
+// int[] arr = {1,5,2,3,2,6,4,7,7};
+
+// void PrintArray(int[] array)
+// {
+//     int count = array.Length;
+
+//     for (int i = 0; i < count; i++)
+//     {
+//         Console.Write($"{array[i]}");
+//     }
+//     Console.WriteLine();
+// }
+
+// void SelectionSort(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         int minPosition = i;
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if (array[j] < array[minPosition]) minPosition = j;
+//         }
+//         int temporary = array[i];
+//         array[i] = array[minPosition];
+//         array[minPosition] = temporary;
+//     }
+    
+// }
+// void ReversArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length / 2; i++)
+//     {
+//         int reversArr = i;
+        
+//         int sum = array[array.Length -1 -i];
+//         array[array.Length -1 -i] = array[reversArr];
+//         array[reversArr] = sum;
+//     }
+// }
+
+// PrintArray(arr);
+// SelectionSort(arr);
+// ReversArray(arr);
+
+// PrintArray(arr);
+
+
+
+
+
+
+
+//Шпаргалка:
 // Массивы - коллекция однотипных данных
 // т.е. несколько значений одного типа данных
 // массивы могут быть любого типа данных
